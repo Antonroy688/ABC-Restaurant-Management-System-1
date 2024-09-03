a33e31e9bc0a14e29eaae3874575d374dd74b4cc
Anton Roy <antonroy68@gmail.com>
2024-09-03 21:09:23 +0530

    an

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-@php($footer_link = \App\Models\GeneralSetting::where('key', 'footer_link')->first())
-@php($footer_text = \App\Models\GeneralSetting::where('key', 'footer_text')->first())
-<footer class="footer text-center">
-               {{$footer_text->value??''}} <a href="{{$footer_link->value??'#'}}">Maestro Innovative Solutions(pvt) Ltd</a>.
-</footer>
